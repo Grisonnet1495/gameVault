f2ca38143e054fb58db242a0d9316dcfb30cceaf
Grisonnet1495 <146077159+Grisonnet1495@users.noreply.github.com>
2025-06-08 23:13:39 +0200

    :fire: Removed unnecessary using

--- gameVaultProject/AuthenticateWindow.xaml.cs
+++ gameVaultProject/AuthenticateWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using gameVaultClassLibrary;
-using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
--- gameVaultProject/MainWindow.xaml.cs
+++ gameVaultProject/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System.Collections.ObjectModel;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using gameVaultClassLibrary;
 
 namespace gameVaultProject
